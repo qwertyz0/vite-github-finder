52b270b67d7fadfdb8829bb2c9db12f55a616ca4
YanisS <yanismac@MacBook-Pro-YanisS.local>
2023-05-28 16:46:17 +0300

    fetching users data(login) and render on page + add spinner

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import Spinner from "../layout/Spinner";
+
+function UserResults() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    const response = await fetch(
+      `${import.meta.env.VITE_APP_GITHUB_URL}/users`,
+      {
+        headers: {
+          Authorization: `token ${import.meta.env.VITE_APP_GITHUB_TOKEN}`,
+        },
+      }
+    );
+
+    const data = await response.json();
+    setUsers(data);
+    setLoading(false);
+  };
+
+  if (!loading) {
+    {
+      /* create grid colomn class style for different monitor size (grid - xl, lg, md) */
+    }
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+        {users.map((user, id) => {
+          return <h3 key={id}>{user.login}</h3>;
+        })}
+      </div>
+    );
+  } else {
+    return <Spinner />;
+  }
+}
+
+export default UserResults;
