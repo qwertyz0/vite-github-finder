1c8e274a5faae0ed819e95599ede22af4a9358e4
YanisS <yanismac@MacBook-Pro-YanisS.local>
2023-06-08 17:56:43 +0300

    add context hook and move fetching function and state to context

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,30 +1,15 @@
 import React from "react";
-import { useEffect, useState } from "react";
+import { useEffect, useContext } from "react";
 import Spinner from "../layout/Spinner";
 import UserItem from "./UserItem";
+import GithubContext from "../../context/github/GithubContext";
 
 function UserResults() {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const {fetchUsers, loading, users} = useContext(GithubContext)
 
   useEffect(() => {
     fetchUsers();
   }, []);
-
-  const fetchUsers = async () => {
-    const response = await fetch(
-      `${import.meta.env.VITE_APP_GITHUB_URL}/users`,
-      {
-        headers: {
-          Authorization: `token ${import.meta.env.VITE_APP_GITHUB_TOKEN}`,
-        },
-      }
-    );
-
-    const data = await response.json();
-    setUsers(data);
-    setLoading(false);
-  };
 
   if (!loading) {
     {
