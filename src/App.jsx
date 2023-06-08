101c888b25bf420a18e5e6fcc34590131f463fec
YanisS <yanismac@MacBook-Pro-YanisS.local>
2023-06-08 17:55:03 +0300

    warap all app in context provider

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,28 @@ import Footer from "./components/layout/Footer";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+import { GithubProvider } from "./context/github/GithubContext";
 
 function App() {
   return (
+    <GithubProvider>
       <Router>
         <div className="flex flex-col justify-between h-screen">
-          <Navbar/>
+          <Navbar />
 
           <main className="container mx-auto px-3 pb-12">
             <Routes>
-              <Route path="/" element={<Home/>} />
-              <Route path="/about" element={<About/>} />
-              <Route path="/notfound" element={<NotFound/>} />
-              <Route path="/*" element={<NotFound/>} />
-
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/notfound" element={<NotFound />} />
+              <Route path="/*" element={<NotFound />} />
             </Routes>
           </main>
 
-          <Footer/>
+          <Footer />
         </div>
       </Router>
+    </GithubProvider>
   );
 }
 
